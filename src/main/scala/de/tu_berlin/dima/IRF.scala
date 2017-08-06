957701c0c99775134064e01cd41f01b15799e146
cristiprg <cristian.prigoana@yahoo.com>
2017-08-06 14:21:04 +0200

    Cosmetic, readability: move housekeeping code in separate method

--- src/main/scala/de/tu_berlin/dima/IRF.scala
+++ src/main/scala/de/tu_berlin/dima/IRF.scala
@@ -9,7 +9,25 @@ import org.apache.spark.ml.wahoo.TrainModel.logError
   * Created by cristiprg on 06.08.17.
   */
 object IRF extends Logging{
-//TODO: 1) toate pointfeature-urile calculate si dupaia batchuri IRF (ca inainte defapt, nu stiu ce plm te-ai complicat)
+
+  def splitFilesIntoTiles(sc: SparkContext, laserPointsFile: String, tilesDirectory: String, batchSize: Int): Int = {
+    val MAX_NUMBER_OF_TILES: Int = 100
+    val laserPointsRDD = sc.textFile(laserPointsFile, MAX_NUMBER_OF_TILES)
+    val N = laserPointsRDD.count() // Get the number of points
+    val T = batchSize // Number of points in every tile
+    val nrTiles = Math.ceil(N.toDouble/T).toInt// Number of tiles, i.e. the number of paritions on this particular RDD
+
+    if (nrTiles > MAX_NUMBER_OF_TILES) {
+      logError("The resulting number of tiles ("+ nrTiles +") is bigger than the maximum allowed ("+MAX_NUMBER_OF_TILES+"), please increase the batch size!")
+      sc.stop()
+      sys.exit(1)
+    }
+
+    laserPointsRDD.coalesce(nrTiles).saveAsTextFile(tilesDirectory) // Save tiles as files, then process them one by one
+    nrTiles
+  }
+
+  //TODO: 1) toate pointfeature-urile calculate si dupaia batchuri IRF (ca inainte defapt, nu stiu ce plm te-ai complicat)
   def main(args: Array[String]) = {
     // Parse args
     val command: String = args(0)
@@ -30,29 +48,14 @@ object IRF extends Logging{
           .set("spark.driver.allowMultipleContexts", "true")
         val sc = new SparkContext(conf)
 
-
         val tilesDirectory = laserPointsFile + "_tiles_folder" // @temporary
-
-        val MAX_NUMBER_OF_TILES: Int = 100
-        val laserPointsRDD = sc.textFile(laserPointsFile, MAX_NUMBER_OF_TILES)
-        val N = laserPointsRDD.count() // Get the number of points
-        val T = batchSize // Number of points in every tile
-        val nrTiles = Math.ceil(N.toDouble/T).toInt// Number of tiles, i.e. the number of paritions on this particular RDD
-
-        if (nrTiles > MAX_NUMBER_OF_TILES) {
-          logError("The resulting number of tiles ("+ nrTiles +") is bigger than the maximum allowed ("+MAX_NUMBER_OF_TILES+"), please increase the batch size!")
-          sc.stop()
-          sys.exit(1)
-        }
-
-        laserPointsRDD.coalesce(nrTiles).saveAsTextFile(tilesDirectory) // Save tiles as files, then process them one by one
+        val nrTiles = splitFilesIntoTiles(sc, laserPointsFile, tilesDirectory, batchSize)
 
         // Declare here in order to avoid code duplicate in train+predict
         val featureAssembler = new VectorAssembler()
           //        .setInputCols(Array("PF_Linearity", "PF_Planarity", "PF_Scattering", "PF_Omnivariance", "PF_Eigenentropy", "PF_Anisotropy", "PF_CurvatureChange", "PF_AbsoluteHeight", "PF_LocalPointDensity3D", "PF_LocalRadius3D", "PF_MaxHeightDiff3D", "PF_HeightVar3D", "PF_LocalRadius2D", "PF_SumEigenvalues2D", "PF_RatioEigenvalues2D", "PF_MAccu2D", "PF_MaxHeightDiffAccu2D", "PF_VarianceAccu2D"))
           .setInputCols(Array("PF_AbsoluteHeight", "PF_Linearity", "PF_Planarity","PF_Scattering", "PF_Omnivariance", "PF_Anisotropy", "PF_Eigenentropy", "PF_CurvatureChange"))
           .setOutputCol("indexedFeatures")
-
 
         command match {
           case "train" =>
